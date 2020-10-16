3341fecfbe1f04c1d348bbfb32ebcafe5de82107
Asi <asierrosa@gmail.com>
2020-10-16 05:52:43 +0200

    a

---
+++ javafx/src/main/java/ehu/isad/Book.java
@@ -0,0 +1,86 @@
+package ehu.isad;
+
+public class Book {
+    String isbn;
+    String title;
+
+    String info_url;
+    String bib_key;
+    String preview_url;
+    String thumbnail_url;
+    BookDetails details;
+
+    public Book(String isbn, String title) {
+        this.isbn = isbn;
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", info_url='" + info_url + '\'' +
+                ", bib_key='" + bib_key + '\'' +
+                ", preview_url='" + preview_url + '\'' +
+                ", thumbnail_url='" + thumbnail_url + '\'' +
+                ", details=" + details +
+                '}';
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getInfo_url() {
+        return info_url;
+    }
+
+    public void setInfo_url(String info_url) {
+        this.info_url = info_url;
+    }
+
+    public String getBib_key() {
+        return bib_key;
+    }
+
+    public void setBib_key(String bib_key) {
+        this.bib_key = bib_key;
+    }
+
+    public String getPreview_url() {
+        return preview_url;
+    }
+
+    public void setPreview_url(String preview_url) {
+        this.preview_url = preview_url;
+    }
+
+    public String getThumbnail_url() {
+        return thumbnail_url;
+    }
+
+    public void setThumbnail_url(String thumbnail_url) {
+        this.thumbnail_url = thumbnail_url;
+    }
+
+    public BookDetails getDetails() {
+        return details;
+    }
+
+    public void setDetails(BookDetails details) {
+        this.details = details;
+    }
+}
